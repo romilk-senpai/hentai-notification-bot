a526cbd6bdd956d92021b71640698f8b95ee72dd
romilk-senpai <81168701+romilk-senpai@users.noreply.github.com>
2024-06-20 13:26:51 +0300

    adding tag now initiates initial quantity parsing

--- controller/telegram/common.go
+++ controller/telegram/common.go
@@ -3,6 +3,7 @@ package tgcontroller
 import (
 	"errors"
 	"fmt"
+	"hentai-notification-bot-re/lib/e"
 )
 
 func (c *Controller) addTagGroup(userHash string, tagGroup string) error {
@@ -34,11 +35,23 @@ func (c *Controller) addTagGroup(userHash string, tagGroup string) error {
 		userInfo.SubscribedTags.Set(expr, make(map[string]int))
 	}
 
+	for _, parser := range c.parsers {
+		remoteQuantity, err := parser.ParseQuantity(tagGroup)
+
+		if err != nil {
+			return e.Wrap("parser error", err)
+		}
+
+		parserMap, _ := userInfo.SubscribedTags.Get(tagGroup)
+
+		parserMap[parser.ParserName()] = remoteQuantity
+	}
+
 	_, err = c.repository.Update(userHash, userInfo)
 
 	if err != nil {
 		return err
 	}
 
-	return c.client.SendMessage(userInfo.ChatID, fmt.Sprintf("Successfully added %s!\nKeep doing what u're doing 😭", expr))
+	return c.client.SendMessage(userInfo.ChatID, fmt.Sprintf("Successfully added <b>%s</b>!\nKeep doing what u're doing 😭", expr))
 }
