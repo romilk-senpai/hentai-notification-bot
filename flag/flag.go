82614817df7e1ac52fd349dc25f9a034ee83ccc8
romilk-senpai <rpakhomov1702@gmail.com>
2024-10-28 19:45:37 +0200

    with webhook flag

--- flag/flag.go
+++ flag/flag.go
@@ -3,11 +3,13 @@ package parseflag
 import "flag"
 
 var (
-	TgToken    string
-	ConfigPath string
+	TgToken     string
+	ConfigPath  string
+	WithWebhook bool
 )
 
 func init() {
 	flag.StringVar(&TgToken, "tg-token", "", "telegram bot token")
-	flag.StringVar(&ConfigPath, "config", "", "Server config")
+	flag.StringVar(&ConfigPath, "config", "", "server config")
+	flag.BoolVar(&WithWebhook, "with-webhook", false, "enable webhook mode")
 }
